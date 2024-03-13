afe2b9c93beef0b8334f31b3527ec93d65eaf3b3
mango <auous2@gmail.com>
2024-03-13 13:39:35 +0900

    메인 콘텐츠 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -3,6 +3,7 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import Header from './Header'
 import Footer from './Footer'
+import Search from './Search'
 
 const Main = ( props ) => {
     return (
