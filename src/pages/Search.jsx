464c4445900086206a167b290fe4583d72208b5f
mango <auous2@gmail.com>
2024-03-19 11:10:21 +0900

    검색 및 유튜브 api 설정하기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,55 +3,31 @@ import { useParams } from 'react-router-dom'
 import Main from '../components/section/Main'
 
 import VideoSearch from '../components/videos/VideoSearch'
-import { fetchFromAPI } from '../utils/api'
 
 const Search = () => {
     const { searchId } = useParams();
     const [ videos, setVideos ] = useState([]);
-    const [ nextPageToken, setNextPageToken ] = useState(null);
-    const [ loading, setLoading ] = useState(true); 
     
     useEffect(() => {
-        setVideos([]);
-        fetchVideos(searchId);
-        setLoading(true);
-    }, [searchId]);
-
-    const fetchVideos = (query, pageToken = '') => {
-        fetchFromAPI(`search?part=snippet&type=video&q=${query}&pageToken=${pageToken}`)
-        .then((data) => {
-            setNextPageToken(data.nextPageToken);
-            setVideos((prevVideos) => [...prevVideos, ...data.items]);
-            setLoading(false);
+        fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+        )
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            setVideos(result.items)
         })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-            setLoading(false); 
-        });
-    };
-        
-    const handleLoadMore = () => {
-        if (nextPageToken) {
-            fetchVideos(searchId, nextPageToken);
-        }
-    };
-
-    const searchPageClass = loading ? 'isLoading' : 'isLoaded';
+        .catch(error => console.log(error));
+        }, [searchId]);
 
     return (
         <Main 
             title = "유투브 검색"
             description="유튜브 검색 결과 페이지입니다.">
             
-            <section id='searchPage' className={searchPageClass}>
-                <h2>🤠 <em>{searchId}</em> 검색 결과입니다.</h2>
+            <section id='searchPage'>
                 <div className="video__inner search">
                     <VideoSearch videos={videos} />
-                </div>
-                <div className="video__more">
-                    {nextPageToken && (
-                        <button onClick={handleLoadMore}>더 보기</button>
-                    )}
                 </div>
             </section>
         </Main>
